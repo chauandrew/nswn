7ffbd3a98d3e39c16f7eda0bab827d57960b529c
Andrew Chau <andrewchau333@gmail.com>
2020-06-29 16:48:40 -0700

    changing project structure to test heroku

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 // Environment setup
 const express = require('express')
-const cors = require('cors')
 const path = require('path')
 const port = process.env.PORT || 4000;
 
-
 const app = express()
-app.use(cors())
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, '/client/build')))
 app.set('port', process.env.PORT || 4000);
 
-
 // Render react components server side
-app.get('/', (req, res) => res.render('index.html')) 
+app.get('/', (req, res) => res.render('index.html'))
 
 // Backend calls
 app.get('/api', (req, res) => res.send('Hello World!'))
