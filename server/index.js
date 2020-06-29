e155bb5950ed91a19d690c57c018383d71dee4fb
Andrew Chau <andrewchau333@gmail.com>
2020-06-29 13:21:59 -0700

    Updated port settings for heroku deployment

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+const port = process.env.PORT || 4000;
+
 
 const app = express()
 app.use(cors())
 app.use(express.static(path.join(__dirname, '../client/build')))
+app.set('port', process.env.PORT || 4000);
+
 
 // Render react components server side
 app.get('/', (req, res) => res.render('index.html')) 
@@ -13,8 +17,4 @@ app.get('/', (req, res) => res.render('index.html'))
 // Backend calls
 app.get('/api', (req, res) => res.send('Hello World!'))
 
-if (process.env.prod_env == 'PRODUCITON') {
-    app.listen(80, () => console.log(`Example app listening at http://localhost:80`))
-} else {
-    app.listen(4000, () => console.log(`Example app listening at http://localhost:4000`))
-}
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
