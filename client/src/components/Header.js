3425d209264362ad2f322faa9f36437bb288d10f
Nathan Sit <nsit.earth@gmail.com>
2020-06-29 11:57:51 -0700

    Added text to the nav bar home page, CSS still needed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,15 +13,21 @@ class Header extends Component {
   render() {
     return (
       <Navbar>
-        <Navbar.Brand href="/">
-          <img src={logo} width="50" height="50"></img>
-        </Navbar.Brand>
         <Nav>
           <Nav.Item>
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#">Our Story</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#">Stories</Nav.Link>
+            <Nav.Link href="#">What To Expect</Nav.Link>
+          </Nav.Item>
+          <Navbar.Brand href="/">
+            <img src={logo} width="50" height="50"></img>
+          </Navbar.Brand>
+          <Nav.Item>
+            <Nav.Link href="#">Got Questions?</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#">Get Started</Nav.Link>
           </Nav.Item>
         </Nav>
       </Navbar>
