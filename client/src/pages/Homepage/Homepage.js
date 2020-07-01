3eeee48633bac4addc1bf8645db33c677060f5f0
dingzhengfuaa <dingzhengfuaa@gmail.com>
2020-07-01 14:58:53 -0700

    updated homepage
    added nswn is title

---
+++ client/src/pages/Homepage/Homepage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Homepage.css';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div id="content" className="container-fluid">
+        <div className="row">
+          <div className="col-md-5 align-self-center" id="nswnIs">
+            <h1>NSWN is </h1>
+            <p id="underscore"></p>
+          </div>
+        </div>
+      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      //   <p className="App-intro">{this.state.apiResponse}</p>
+      // </div>
+    );
+  }
+}
+
+export default Homepage;
