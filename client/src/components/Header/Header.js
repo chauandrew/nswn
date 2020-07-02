1be494c48d560b50c19c63c13f545ee212f71e60
Andrew Chau <andrewchau333@gmail.com>
2020-07-01 17:47:38 -0700

    Created separate mobile/desktop header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,26 +6,63 @@ import './Header.css';
 
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+
+  // Add event listener on window resize
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  // Remove the listener when the component is not mounted anymore
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
   render() {
-    return (
-        <Nav className='m-auto'>
-          <Nav.Item>
-            <Nav.Link href="/stories" className="text-secondary">OUR STORIES</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/about" className="text-secondary">WHAT TO EXPECT</Nav.Link>
-          </Nav.Item>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="logo" width="50" height="50"></img>
+    const { width } = this.state;
+    const isMobile = width <= 768;
+    if (!isMobile) {
+      // Desktop View
+      return (
+        <Navbar className='m-auto' fixed='top' expand='lg'>
+          <Nav.Link href='/stories' className='text-secondary'>OUR STORIES</Nav.Link>
+          <Nav.Link href='/about' className='text-secondary'>WHAT TO EXPECT</Nav.Link>
+          <Navbar.Brand href='/'>
+            <img src={logo} alt='logo' width='50' height='50'></img>
           </Navbar.Brand>
-          <Nav.Item>
-            <Nav.Link href="/course-101" className="text-secondary">GOT QUESTIONS?</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/get-connected" className="text-dark font-weight-bold">GET STARTED</Nav.Link>
-          </Nav.Item>
-        </Nav>
-    );
+          <Nav.Link href='/course-101' className='text-secondary'>GOT QUESTIONS?</Nav.Link>
+          <Nav.Link href='/get-connected' className='text-dark font-weight-bold'>GET STARTED</Nav.Link>
+        </Navbar>
+      );
+    } else {
+      // Mobile View
+      return (
+        <Navbar collapseOnSelect fixed='top' expand='lg'>
+          <Navbar.Brand href='/'>
+            <img src={logo} alt='logo' width='50' height='50'></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Link href='/' className='text-secondary'>HOME</Nav.Link>
+              <Nav.Link href='/stories' className='text-secondary'>OUR STORIES</Nav.Link>
+              <Nav.Link href='/about' className='text-secondary'>WHAT TO EXPECT</Nav.Link>
+              <Nav.Link href='/course-101' className='text-secondary'>GOT QUESTIONS?</Nav.Link>
+              <Nav.Link href='/get-connected' className='text-dark font-weight-bold'>GET STARTED</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      )
+    }
   }
 }
 
