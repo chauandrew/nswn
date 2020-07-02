134520f96388556bcf95c56378d24d985d075a4e
YiyueMaggieMao <myykitty711@gmail.com>
2020-07-02 01:24:09 -0700

    Improved spacing and sizing of the content section

--- client/src/pages/Course101/Course101.js
+++ client/src/pages/Course101/Course101.js
@@ -11,12 +11,14 @@ const Course101 = () => {
                 
                 <div className="c101-intro">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Et tortor consequat id porta nibh venenatis cras sed. Turpis massa tincidunt dui ut ornare lectus sit amet est. Massa ultricies mi quis hendrerit dolor magna eget est. Faucibus et molestie ac feugiat. Congue nisi vitae suscipit tellus mauris a diam maecenas. Volutpat est velit egestas dui id ornare arcu odio ut. Sodales neque sodales ut etiam sit amet. Ut pharetra sit amet aliquam id diam maecenas ultricies mi. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Magna fermentum iaculis eu non diam.</p>
                     <img id="c101-vid" src="https://redeeminggod.com/wp-content/uploads/2015/10/truth-bible.jpg" alt="Oops"/> 
                 </div>
                 
                 <div className="c101-info">
                     <div className="c101-info-sub" id="101-info-left">
-                        <p>Course 101, a seven-week course developed by Gracepoint Ministries, gives an
+                        <p>Course 101, a seven-week course developed by{'\u00A0'}
+                        <a href="https://www.gracepointonline.org/">Gracepoint Ministries</a>, gives an
                         intellectual basis for answers to some of the most commonly asked questions about
                         the Christian faith and addresses misconceptions about God, Jesus and the Bible.
                         Both those who have experience with Christianity and those just beginning to consider it
