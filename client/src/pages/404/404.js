9ac5e62601cfe7e537d82851d8efe603efcc4e8d
Andrew Chau <andrewchau333@gmail.com>
2020-07-02 17:05:06 -0700

    Basic 404 page styling for fun

--- client/src/pages/404/404.js
+++ client/src/pages/404/404.js
@@ -4,7 +4,16 @@ import './404.css';
 class Error404 extends Component {
     render() {
         return (
-            <h1>Error page not found!</h1>
+        <div class="page-content error404-wrapper">
+            <div class="error404 text-center container">
+                <h1>Lost?</h1>
+                <p>
+                Jesus said to him, "I am the way, and the truth, and the life.
+                No one comes to the Father except through me.""
+                </p>
+                <h6><i>John 14:6</i></h6>
+            </div>
+        </div>
         );
     }
 }
