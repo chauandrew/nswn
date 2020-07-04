9a6123fa134a1620a2ab0df35c312e4bc5da8c25
Andrew Chau <andrewchau333@gmail.com>
2020-07-04 15:06:12 -0700

    Updated story page using Bootstrap Cards and added generic placeholder images

--- client/src/pages/Stories/Stories.js
+++ client/src/pages/Stories/Stories.js
@@ -1,141 +1,30 @@
 import React, { Component } from 'react';
+import CardColumns from 'react-bootstrap/CardColumns';
+import Container from 'react-bootstrap/Container';
+import StoryCard from '../../components/StoryCard/StoryCard';
+import HeroImage from '../../components/HeroImage/HeroImage';
+
+import * as Placeholders from '../../assets/placeholders';
 import './Stories.css';
 
 class Stories extends Component {
     render() {
+        var placeholderText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         return (
             <div>
-                <h1 class="header">Our Stories</h1>
-                <img></img>
-                <ul class="flex-container">
-                    <li class="profile">
-                        <img class="profPic"></img>
-                        <p class="profDesc">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing 
-                            elit, sed do eiusmod tempor incididunt ut labore 
-                            et dolore magna aliqua. Ut enim ad minim veniam, 
-                            quis nostrud exercitation ullamco laboris nisi ut 
-                            aliquip ex ea commodo consequat. Duis aute irure 
-                            dolor in reprehenderit in voluptate velit esse 
-                            cillum dolore eu fugiat nulla pariatur. Excepteur 
-                            sint occaecat cupidatat non proident, sunt in 
-                            culpa qui officia deserunt mollit anim id est 
-                            laborum.
-                        </p>
-                    </li>
-
-                    <li class="profile">
-                        <img class="profPic"></img>
-                        <p class="profDesc">
-                            Sed ut perspiciatis unde omnis iste natus error 
-                            sit voluptatem accusantium doloremque laudantium, 
-                            totam rem aperiam, eaque ipsa quae ab illo 
-                            inventore veritatis et quasi architecto beatae 
-                            vitae dicta sunt explicabo. Nemo enim ipsam 
-                            voluptatem quia voluptas sit aspernatur aut odit 
-                            aut fugit, sed quia consequuntur magni dolores eos 
-                            qui ratione voluptatem sequi nesciunt. Neque 
-                            porro quisquam est, qui dolorem ipsum quia dolor 
-                            sit amet, consectetur, adipisci velit, sed quia 
-                            non numquam eius modi tempora incidunt ut labore 
-                            et dolore magnam aliquam quaerat voluptatem. Ut 
-                            enim ad minima veniam, quis nostrum 
-                            exercitationem ullam corporis suscipit 
-                            laboriosam, nisi ut aliquid ex ea commodi 
-                            consequatur? Quis autem vel eum iure 
-                            reprehenderit qui in ea voluptate velit esse quam 
-                            nihil molestiae consequatur, vel illum qui 
-                            dolorem eum fugiat quo voluptas nulla pariatur?
-                        </p>
-                    </li>
-
-                    <li class="profile">
-                        <img class="profPic"></img>
-                        <p class="profDesc">
-                            At vero eos et accusamus et iusto odio dignissimos 
-                            ducimus qui blanditiis praesentium voluptatum 
-                            deleniti atque corrupti quos dolores et quas 
-                            molestias excepturi sint occaecati cupiditate non 
-                            provident, similique sunt in culpa qui officia 
-                            deserunt mollitia animi, id est laborum et dolorum 
-                            fuga. Et harum quidem rerum facilis est et 
-                            expedita distinctio. Nam libero tempore, cum 
-                            soluta nobis est eligendi optio cumque nihil 
-                            impedit quo minus id quod maxime placeat facere 
-                            possimus, omnis voluptas assumenda est, omnis 
-                            dolor repellendus. Temporibus autem quibusdam et 
-                            aut officiis debitis aut rerum necessitatibus 
-                            saepe eveniet ut et voluptates repudiandae sint et 
-                            molestiae non recusandae. Itaque earum rerum hic 
-                            tenetur a sapiente delectus, ut aut reiciendis 
-                            voluptatibus maiores alias consequatur aut 
-                            perferendis doloribus asperiores repellat.
-                        </p>
-                    </li>
-                
-                    <li class="profile">
-                        <img class="profPic"></img>
-                        <p class="profDesc">
-                            At in tellus integer feugiat scelerisque varius 
-                            morbi enim. Sed viverra ipsum nunc aliquet 
-                            bibendum enim. Congue eu consequat ac felis donec 
-                            et. Porttitor massa id neque aliquam vestibulum 
-                            morbi blandit cursus risus. Nisl nisi scelerisque 
-                            eu ultrices vitae auctor eu augue ut. At imperdiet 
-                            dui accumsan sit. Aliquam faucibus purus in massa 
-                            tempor. Id ornare arcu odio ut sem nulla pharetra 
-                            diam sit. Bibendum est ultricies integer quis 
-                            auctor elit sed vulputate mi. Malesuada 
-                            pellentesque elit eget gravida cum sociis natoque 
-                            penatibus. Mauris sit amet massa vitae tortor 
-                            condimentum lacinia quis. Sem viverra aliquet eget 
-                            sit amet. Commodo odio aenean sed adipiscing diam 
-                            donec adipiscing tristique risus.
-                        </p>
-                    </li>
-
-                    <li class="profile">
-                        <img class="profPic"></img>
-                        <p class="profDesc">
-                            Dictumst quisque sagittis purus sit amet volutpat. 
-                            Magna etiam tempor orci eu. Vulputate odio ut enim 
-                            blandit volutpat maecenas volutpat blandit 
-                            aliquam. Quis varius quam quisque id diam vel. 
-                            Eget felis eget nunc lobortis mattis aliquam 
-                            faucibus. Sed risus pretium quam vulputate 
-                            dignissim. Aliquet lectus proin nibh nisl 
-                            condimentum. Phasellus egestas tellus rutrum 
-                            tellus pellentesque eu. Congue nisi vitae suscipit 
-                            tellus mauris a diam maecenas. Nullam non nisi est 
-                            sit. Nunc id cursus metus aliquam. Morbi blandit 
-                            cursus risus at.
-                        </p>
-                    </li>
-
-                    <li class="profile">
-                        <img class="profPic"></img>
-                        <p class="profDesc">
-                            Orci sagittis eu volutpat odio facilisis. Proin 
-                            libero nunc consequat interdum varius sit. 
-                            Facilisis leo vel fringilla est ullamcorper eget 
-                            nulla. Consectetur adipiscing elit pellentesque 
-                            habitant morbi tristique senectus. Blandit 
-                            volutpat maecenas volutpat blandit aliquam etiam 
-                            erat velit. Et netus et malesuada fames ac. Nulla 
-                            facilisi cras fermentum odio eu feugiat. 
-                            Pellentesque habitant morbi tristique senectus et 
-                            netus. Aliquam id diam maecenas ultricies mi eget 
-                            mauris. Senectus et netus et malesuada fames ac 
-                            turpis. In cursus turpis massa tincidunt. Faucibus 
-                            a pellentesque sit amet. Tempor commodo 
-                            ullamcorper a lacus vestibulum sed arcu non odio. 
-                            Nisl vel pretium lectus quam id leo in. Sit amet 
-                            nisl suscipit adipiscing bibendum est ultricies 
-                            integer. Diam volutpat commodo sed egestas egestas 
-                            fringilla.
-                        </p>
-                    </li>
-                </ul>
+                <HeroImage value="OUR STORY" src={Placeholders.standard}/>
+                <Container fluid className="card-container">
+                    <CardColumns>
+                        <StoryCard title="Jeff Ding" value={placeholderText} src={Placeholders.standard} />
+                        <StoryCard title="Jeff Ding" value={placeholderText} src={Placeholders.wide} />
+                        <StoryCard title="Jeff Ding" value={placeholderText} src={Placeholders.vertical} />
+                        <StoryCard title="Jeff Ding" value={placeholderText} src={Placeholders.wide} />
+                        <StoryCard title="Jeff Ding" value={placeholderText} src={Placeholders.standard} />
+                        <StoryCard title="Jeff Ding" value={placeholderText} src={Placeholders.wide} />
+                        <StoryCard title="Jeff Ding" value={placeholderText} src={Placeholders.standard} />
+                        <StoryCard title="Jeff Ding" value={placeholderText} src={Placeholders.vertical} />
+                    </CardColumns>
+                </Container>
             </div>
         );
     }
