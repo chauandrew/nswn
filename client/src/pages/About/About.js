ea9c8670a91e81259d09aef6c84c3e43d81b8f2a
dingzhengfuaa <dingzhengfuaa@gmail.com>
2020-07-13 14:55:59 -0700

    about page layout and video
    created about page design, with NSWN banner and video

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -4,7 +4,14 @@ import './About.css';
 class About extends Component {
     render() {
         return (
-            <h1>About</h1>
+            <div>
+                <div className="container-fluid">
+                    <div className="text">What is NSWN?</div>
+                </div>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/YDqYu-57PNQ" allowfullscreen></iframe>
+                </div>
+            </div>
         );
     }
 }
